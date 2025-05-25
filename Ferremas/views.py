f877ce1b8ecfea80193b572791ea7367d7dd54ed
VicenGonzalezDuoc <vicen.gonzalezl@duocuc.cl>
2025-05-25 17:08:29 -0400

    home and firebase database

---
+++ Ferremas/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import pyrebase 
+
+config={
+    "apiKey": "AIzaSyD8ccAxOcOftM2KVcH28Di7rvpYW8Mp-nM",
+    "authDomain": "ferremas-292f7.firebaseapp.com",
+    "databaseURL": "https://ferremas-292f7-default-rtdb.firebaseio.com",
+    "projectId": "ferremas-292f7",
+    "storageBucket": "ferremas-292f7.firebasestorage.app",
+    "messagingSenderId": "837938741466",
+    "appId": "1:837938741466:web:2e63e016318b97ec2525e3",
+}
